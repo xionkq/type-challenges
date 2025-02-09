60f6dac614cbfc55a651e8da8a564246854ddc3b
xionkq <3030199850@qq.com>
2025-02-09 17:22:26 +0800

    feat: solve 00898-easy-includes

---
+++ solutions/00898-easy-includes/solution.ts
@@ -0,0 +1,56 @@
+/*
+  898 - Includes
+  -------
+  by null (@kynefuk) #简单 #array
+
+  ### 题目
+
+  在类型系统里实现 JavaScript 的 `Array.includes` 方法，这个类型接受两个参数，返回的类型要么是 `true` 要么是 `false`。
+
+  例如：
+
+  ```ts
+  type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/898/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+/*
+笔记:
+  1. 需要使用遍历法解这道题，通过 infer 取出数组中所有元素，一一与目标元素对比，判断结果
+  2. 取元素时每次只取出数组第一个元素对比，若对比失败则递归剩余元素
+  3. ts 类型中似乎没有判等操作（===），只能 extends，这样对比其实并不规范，此时需要借助 type-challenges 中的 Equal 类型
+  （这种题解太难想到了吧）
+*/
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R] ? (Equal<U, F> extends true ? true : Includes<R, U>) : false
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>,
+]
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/898/answer/zh-CN
+  > 查看解答：https://tsch.js.org/898/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
