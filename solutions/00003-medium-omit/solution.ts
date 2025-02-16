f3b037d63ad9bd26b119d95a4bf44f6c1b41af6b
xionkq <3030199850@qq.com>
2025-02-16 20:00:06 +0800

    feat: solve 00003-medium-omit

---
+++ solutions/00003-medium-omit/solution.ts
@@ -0,0 +1,84 @@
+/*
+  3 - 实现 Omit
+  -------
+  by Anthony Fu (@antfu) #中等 #union #built-in
+
+  ### 题目
+
+  不使用 `Omit` 实现 TypeScript 的 `Omit<T, K>` 泛型。
+
+  `Omit` 会创建一个省略 `K` 中字段的 `T` 对象。
+
+  例如：
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+  const todo: TodoPreview = {
+    completed: false,
+  }
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/3/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+/*
+笔记:
+  1. 本例中的类型不是普通类型，是一个对象类型，和对象字面量作为类型是一样的
+  2. 要遍历对象类型需要使用 keyof 关键字取出所有 key
+  3. as 关键字可以用于映射类型，用于更改键的名称
+*/
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+  Expect<Equal<Expected3, MyOmit<Todo1, 'description' | 'completed'>>>,
+]
+
+// @ts-expect-error
+type error = MyOmit<Todo, 'description' | 'invalid'>
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Todo1 {
+  readonly title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+  completed: boolean
+}
+
+interface Expected2 {
+  title: string
+}
+
+interface Expected3 {
+  readonly title: string
+}
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/3/answer/zh-CN
+  > 查看解答：https://tsch.js.org/3/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
