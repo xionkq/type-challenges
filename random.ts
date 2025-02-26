4619c47a6c7f43a12ec4a156685c20e1074b4c5b
xionkq <3030199850@qq.com>
2025-02-26 20:13:53 +0800

    chore: generate randomly question dir

--- random.ts
+++ random.ts
@@ -42,6 +42,20 @@ async function generateRandomProblem() {
     const randomIndex = Math.floor(Math.random() * all.length)
     randomProblem = all[randomIndex]
   }
+
+  const dirPath = path.join(__dirname, 'solutions', randomProblem)
+  try {
+    // 新建目录
+    fs.mkdirSync(dirPath, { recursive: true })
+    const fileAPath = path.join(dirPath, 'solution.ts')
+    const fileBPath = path.join(dirPath, 'meta.json')
+    fs.writeFileSync(fileAPath, '')
+    fs.writeFileSync(fileBPath, '')
+
+    console.log(`目录已成功创建: ${dirPath}`)
+  } catch (err) {
+    console.error(`创建目录时出错: ${err}`)
+  }
   console.log(randomProblem)
   return randomProblem
 }
