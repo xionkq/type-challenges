bcd749dd6275933bef3f324a85559895b0d6cfb0
xionkq <3030199850@qq.com>
2025-05-09 21:21:50 +0800

    feat: solve 02070-medium-drop-char

---
+++ solutions/02070-medium-drop-char/solution.ts
@@ -0,0 +1,48 @@
+/*
+  2070 - Drop Char
+  -------
+  by CaptainOfPhB (@CaptainOfPhB) #中等 #template-literal #infer
+
+  ### 题目
+
+  从字符串中剔除指定字符。
+
+  例如：
+
+  ```ts
+  type Butterfly = DropChar<' b u t t e r f l y ! ', ' '> // 'butterfly!'
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/2070/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+/*
+笔记:
+  1. 使用模板字符串 + infer 推断可以轻松解决
+*/
+type DropChar<S, C> = S extends `${infer F}${infer R}` ? F extends C ? DropChar<R, C> : `${F}${DropChar<R, C>}` : S
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type A = DropChar<'butter fly!', ' '>
+let a: A
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
+  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/2070/answer/zh-CN
+  > 查看解答：https://tsch.js.org/2070/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
