fba7799e772d22a22ba1000ffca89443bed8385d
xionkq <3030199850@qq.com>
2025-06-20 17:29:55 +0800

    feat: solve 04499-medium-chunk

---
+++ solutions/04499-medium-chunk/solution.ts
@@ -0,0 +1,54 @@
+/*
+  4499 - Chunk
+  -------
+  by キリサメ qianxi (@qianxi0410) #medium #tuple
+
+  ### Question
+
+  Do you know `lodash`? `Chunk` is a very useful function in it, now let's implement it.
+  `Chunk<T, N>` accepts two required type parameters, the `T` must be a `tuple`, and the `N` must be an `integer >=1`
+
+  ```ts
+  type exp1 = Chunk<[1, 2, 3], 2> // expected to be [[1, 2], [3]]
+  type exp2 = Chunk<[1, 2, 3], 4> // expected to be [[1, 2, 3]]
+  type exp3 = Chunk<[1, 2, 3], 1> // expected to be [[1], [2], [3]]
+  ```
+
+  > View on GitHub: https://tsch.js.org/4499
+*/
+
+/* _____________ Your Code Here _____________ */
+/*
+笔记:
+  1. 将结果作为第三个泛型参数递归，就是判断条件有点多
+*/
+type Chunk<T extends any[], N extends number, Res extends any[] = []> = T extends []
+  ? Res
+  : T extends [infer F, ...infer R]
+    ? Res extends []
+      ? Chunk<R, N, [[F]]>
+      : Res extends [...infer ResF, infer L extends any[]]
+        ? L['length'] extends N
+          ? Chunk<R, N, [...Res, [F]]>
+          : Chunk<R, N, [...ResF, [...L, F]]>
+        : never
+    : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Chunk<[], 1>, []>>,
+  Expect<Equal<Chunk<[1, 2, 3], 1>, [[1], [2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3], 2>, [[1, 2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
+  Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4499/answer
+  > View solutions: https://tsch.js.org/4499/solutions
+  > More Challenges: https://tsch.js.org
+*/
