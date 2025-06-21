1460226588e0834a1685f5e169baf901fee33122
xionkq <3030199850@qq.com>
2025-06-21 20:42:48 +0800

    feat: solve 02688-medium-startswith

--- solutions/02693-medium-endswith/solution.ts
+++ solutions/02693-medium-endswith/solution.ts
@@ -23,24 +23,24 @@
 笔记:
   1. 字符串反转然后当作 StartsWith 处理
 */
-// type ReverseString<T extends string> = T extends `${infer F}${infer R}` ? `${ReverseString<R>}${F}` : ''
-//
-// type StartsWith<T extends string, U extends string> = U extends ''
-//   ? true
-//   : T extends `${infer TF}${infer TR}`
-//     ? U extends `${infer UF}${infer UR}`
-//       ? TF extends UF ? StartsWith<TR, UR>
-//         : false
-//       : false
-//     : false
-//
-// type EndsWith<T extends string, U extends string> = StartsWith<ReverseString<T>, ReverseString<U>>
+type ReverseString<T extends string> = T extends `${infer F}${infer R}` ? `${ReverseString<R>}${F}` : ''
 
-/*
-笔记:
-  1. 牛逼，我咋就想不到呢
-*/
-type EndsWith<T extends string, U extends string> = T extends `${string}${U}` ? true: false
+type StartsWith<T extends string, U extends string> = U extends ''
+  ? true
+  : T extends `${infer TF}${infer TR}`
+    ? U extends `${infer UF}${infer UR}`
+      ? TF extends UF ? StartsWith<TR, UR>
+        : false
+      : false
+    : false
+
+type EndsWith<T extends string, U extends string> = StartsWith<ReverseString<T>, ReverseString<U>>
+
+// /*
+// 笔记:
+//   1. 牛逼，我咋就想不到呢
+// */
+// type EndsWith<T extends string, U extends string> = T extends `${string}${U}` ? true: false
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
